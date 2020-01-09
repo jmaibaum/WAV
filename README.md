207e176771778815ac7ced0ebd9a7111172ec7aa
Fluhzar <fluhzar@pm.me>
2020-01-08 16:11:31 -0800

    Main code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WAV
+
+This is a crate for reading in and writing out wave files. It supports bit
+depths of 8, 16, and 24 bits, and any number of channels.
---
+++ tests/test.rs
@@ -0,0 +1,11 @@
+extern crate wav;
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn test_wav() {
+		let (h,b) = wav::read_file(std::path::Path::new("data/sine.wav")).unwrap();
+
+		wav::write_wav(h, b, std::path::Path::new("data/output.wav")).unwrap();
+	}
+}
