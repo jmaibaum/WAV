e38663256b480b442b2c84fcefea479c7fc36f37
Fluhzar <fluhzar@pm.me>
2021-03-05 13:35:11 -0800

    Ran `cargo fmt`

--- benches/read.rs
+++ benches/read.rs
@@ -1,35 +1,34 @@
 #![feature(test)]
-use wav::read;
 use std::fs::File;
-use std::io::Read;
 use std::io::Cursor;
+use std::io::Read;
+use wav::read;
 
 extern crate test;
-use test::{Bencher , black_box};
+use test::{black_box, Bencher};
 
-
-fn read_bench(b: &mut Bencher , data: &[u8]){
-    b.iter(||{
+fn read_bench(b: &mut Bencher, data: &[u8]) {
+    b.iter(|| {
         black_box(read(&mut Cursor::new(data))).unwrap();
     })
 }
 
-macro_rules! create_read_bench{
+macro_rules! create_read_bench {
     ($name: ident , $path: literal) => {
         #[bench]
-        fn $name(b: &mut Bencher){
+        fn $name(b: &mut Bencher) {
             let mut data = Vec::new();
             File::open($path).unwrap().read_to_end(&mut data).unwrap();
-            read_bench(b , data.as_slice());
+            read_bench(b, data.as_slice());
         }
-    }
+    };
 }
 
-create_read_bench!(read_sine , "data/sine.wav");
-create_read_bench!(read_sine_8bit_48khz , "data/sine_8bit48kHz.wav");
-create_read_bench!(read_sine_16bit_48khz , "data/sine_16bit48kHz.wav");
-create_read_bench!(read_sine_24bit_48khz , "data/sine_24bit48kHz.wav");
-create_read_bench!(read_sine_32bit_float_48khz , "data/sine_32bitfloat48kHz.wav");
+create_read_bench!(read_sine, "data/sine.wav");
+create_read_bench!(read_sine_8bit_48khz, "data/sine_8bit48kHz.wav");
+create_read_bench!(read_sine_16bit_48khz, "data/sine_16bit48kHz.wav");
+create_read_bench!(read_sine_24bit_48khz, "data/sine_24bit48kHz.wav");
+create_read_bench!(read_sine_32bit_float_48khz, "data/sine_32bitfloat48kHz.wav");
 
 /*#[bench]
 fn read_sine(b: &mut Bencher){
