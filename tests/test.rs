5c6d9ca5363fd2fe428b457b246cf349ba31c08f
Fluhzar <fluhzar@pm.me>
2021-05-15 02:53:56 -0700

    Changed tests to use `std::io::Cursor`

--- tests/test.rs
+++ tests/test.rs
@@ -1,89 +1,5 @@
 use std::io;
 
-struct Data(Vec<u8>);
-
-impl Data {
-    fn reader(&self) -> EmulatedReader {
-        EmulatedReader {
-            bytes: &self.0,
-            pos: 0,
-        }
-    }
-
-    fn writer(&mut self) -> EmulatedWriter {
-        EmulatedWriter {
-            bytes: &mut self.0,
-            pos: 0,
-        }
-    }
-}
-
-struct EmulatedReader<'a> {
-    bytes: &'a[u8],
-    pos: usize,
-}
-
-impl<'a> io::Read for EmulatedReader<'a> {
-    fn read(&mut self, buf: &mut[u8]) -> io::Result<usize> {
-        let mut num_read = 0;
-
-        for (out, inp) in buf.iter_mut().zip(self.bytes[self.pos..].iter()) {
-            *out = *inp;
-            num_read += 1;
-        }
-
-        self.pos += num_read;
-        Ok(num_read)
-    }
-}
-
-impl<'a> io::Seek for EmulatedReader<'a> {
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        match pos {
-            io::SeekFrom::Start(n) => self.pos = n as usize,
-            io::SeekFrom::End(n) => self.pos = self.bytes.len() - n as usize,
-            io::SeekFrom::Current(n) => self.pos += n as usize,
-        }
-
-        Ok(self.pos as u64)
-    }
-}
-
-struct EmulatedWriter<'a> {
-    bytes: &'a mut[u8],
-    pos: usize,
-}
-
-impl<'a> io::Write for EmulatedWriter<'a> {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let mut num_written = 0;
-
-        for (out, inp) in self.bytes[self.pos..].iter_mut().zip(buf.iter()) {
-            *out = *inp;
-            num_written += 1;
-        }
-
-        self.pos += num_written;
-        Ok(num_written)
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        Ok(())
-    }
-}
-
-impl<'a> io::Seek for EmulatedWriter<'a> {
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        match pos {
-            io::SeekFrom::Start(n) => self.pos = n as usize,
-            io::SeekFrom::End(n) => self.pos = self.bytes.len() - n as usize,
-            io::SeekFrom::Current(n) => self.pos += n as usize,
-        }
-
-        Ok(self.pos as u64)
-    }
-}
-
 macro_rules! impl_wav_test {
     ( $(
         $F:ident {
@@ -101,10 +17,10 @@ macro_rules! impl_wav_test {
         #[test]
         fn $F() {
             let raw: &[u8] = include_bytes!($f);
-            let mut inp = Data(raw.into());
-            let mut out = Data(vec![0u8 ; raw.len()]);
+            let mut inp: io::Cursor<Vec<u8>> = io::Cursor::new(raw.into());
+            let mut out: io::Cursor<Vec<u8>> = io::Cursor::new(vec![0u8 ; raw.len()]);
 
-            let (header, data) = wav::read(&mut inp.reader()).unwrap();
+            let (header, data) = wav::read(&mut inp).unwrap();
 
             assert_eq!(header.audio_format, $af);
             assert_eq!(header.channel_count, $cc);
@@ -114,9 +30,9 @@ macro_rules! impl_wav_test {
             assert_eq!(header.bytes_per_second, $by_p_se);
             assert!($ibf(&data));
 
-            wav::write(header, &data, &mut out.writer()).unwrap();
+            wav::write(header, &data, &mut out).unwrap();
 
-            for (i, o) in inp.0.drain(0..).zip(out.0.drain(0..)) {
+            for (i, o) in inp.into_inner().drain(0..).zip(out.into_inner().drain(0..)) {
                 assert_eq!(i, o);
             }
         }
